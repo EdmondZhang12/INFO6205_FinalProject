99e82fa059b42a67a914e203396dcb84a1b758da
Edmond <edmondzhang1212@gmail.com>
2022-04-18 16:25:23 -0400

    feat: Basic part of project

---
+++ TicTacToe/ArtificialIntelligence/Algorithms.java
@@ -0,0 +1,37 @@
+package ArtificialIntelligence;
+
+import TicTacToe.Board;
+
+/**
+ * Uses various algorithms to play Tic Tac Toe.
+ */
+public class Algorithms {
+
+    /**
+     * Algorithms cannot be instantiated.
+     */
+    private Algorithms() {}
+
+    /**
+     * Play a random move.
+     * @param board     the Tic Tac Toe board to play on
+     */
+    public static void random (Board board) {
+        Random.run(board);
+    }
+
+    /**
+     * Play using the MiniMax Algorithm.
+     * @param board     the Tic Tac Toe board to play on
+     */
+    public static void miniMax (Board board) {
+        MiniMax.run(board.getTurn(), board, Double.POSITIVE_INFINITY);
+    }
+
+    /**
+     * Play using the MiniMax algorithm. Include a depth limit.
+     * @param board     the Tic Tac Toe board to play on
+     * @param ply       the maximum depth
+     */
+
+}
