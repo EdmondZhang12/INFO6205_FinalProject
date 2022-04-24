9131788b234048bb4123db6bcf15e6a298d63314
HenryXLiu <1417249519@qq.com>
2022-04-24 13:51:43 -0400

    Create GUITest.java

---
+++ src/test/java/TicTacToe/GUITest.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GUITest {
+    @Test
+    void contextLoads() {
+    }
+}
