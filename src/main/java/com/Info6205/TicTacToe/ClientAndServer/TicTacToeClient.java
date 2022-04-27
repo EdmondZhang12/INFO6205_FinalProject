93140d86c54743f02b49e3f2c8de9e5f15730010
HenryXLiu <1417249519@qq.com>
2022-04-26 22:08:41 -0400

    Update MainInterface.java

--- src/main/java/com/Info6205/TicTacToe/ClientAndServer/TicTacToeClient.java
+++ src/main/java/com/Info6205/TicTacToe/ClientAndServer/TicTacToeClient.java
@@ -79,6 +79,9 @@ public class TicTacToeClient extends JPanel implements Runnable{
      */
     private void addReturnBtn() {
         JButton returnBtn = new JButton("Back <");
+        returnBtn.setPreferredSize(new Dimension(200,50));
+        Font font = new Font(" ", Font.PLAIN, 15);
+        returnBtn.setFont(font);
         returnBtn.addActionListener(e -> {
             if(!board.isGameOver() ) {
                 JOptionPane pane = new JOptionPane();
